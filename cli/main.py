442c0c4b6a50303e541ed832e3b1ec3b0fa88a33
c2t-r <80561604+c2t-r@users.noreply.github.com>
2026-04-14 14:00:13 +0900

    lint: auto fix

--- cli/main.py
+++ cli/main.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 import sys
-from cli import lundump
-from cli import lparser
 
-def main():
-    if len(sys.argv) < 2:
-        print(f"Usage: {sys.argv[0]} <input_file>", file=sys.stderr)
-        sys.exit(1)
+from cli import lparser, lundump
 
-    lc = lundump.LuaUndump()
-    print(sys.argv[1])
-    chunk = lc.loadFile(sys.argv[1])
 
-    lc.print_dissassembly()
+def main() -> None:
+  if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} <input_file>", file=sys.stderr)
+    sys.exit(1)
 
-    lp = lparser.LuaDecomp(chunk)
+  lc = lundump.LuaUndump()
+  print(sys.argv[1])
+  chunk = lc.loadFile(sys.argv[1])
 
-    print("\n==== [[" + str(chunk.name) + "'s pseudo-code]] ====\n")
-    print(lp.getPseudoCode())
+  lc.print_dissassembly()
+
+  lp = lparser.LuaDecomp(chunk)
+
+  print("\n==== [[" + str(chunk.name) + "'s pseudo-code]] ====\n")
+  print(lp.getPseudoCode())
+
 
 if __name__ == "__main__":
-    main()
+  main()
